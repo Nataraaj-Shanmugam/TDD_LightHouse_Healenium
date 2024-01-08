5165b851f6cd96b528db6db7cf89c3fc23e8f18b
TestLeafSDET <nataraajshanmugam08@gmail.com>
2024-01-09 03:25:52 +0530

    change in gsheet auth type

--- src/main/java/functional/utilities/DataProviderUtility.java
+++ src/main/java/functional/utilities/DataProviderUtility.java
@@ -1,19 +1,22 @@
 package functional.utilities;
 
-import functional.genericKeywords.GenericKeywords;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
+import functional.genericKeywords.GenericKeywords;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -128,7 +131,7 @@ public class DataProviderUtility {
         /**
          * The file path for the credentials JSON file.
          */
-        private static final String CREDENTIALS_FILE_PATH = "./Credentials.json";
+        private static final String CREDENTIALS_FILE_PATH = "./ServiceAccount.json";
 
         /**
          * Retrieves credentials for Google Sheets API access.
@@ -140,6 +143,7 @@ public class DataProviderUtility {
         private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
                 throws IOException {
 
+            //for OAUTH 2
             InputStream in = DataProviderUtility.class.getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH);
             if (in == null) {
                 throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
@@ -158,17 +162,51 @@ public class DataProviderUtility {
         }
 
         /**
+         * Retrieves credentials for Google Sheets API access.
+         *
+         * @return Credential object for accessing Google Sheets.
+         * @throws IOException If the credentials file is not found.
+         */
+        private static Credential getCredentials()
+                throws IOException {
+
+            return GoogleCredential.fromStream(DataProviderUtility.class.getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH))
+                    .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS_READONLY));
+        }
+
+        /**
+         * Creates and returns a new {@link Sheets} service instance. This service can be used
+         * for interacting with the Google Sheets API. The method establishes a new connection
+         * with the Google Sheets service, using the credentials obtained from the {@code getCredentials()} method.
+         * It sets the application name to "Test Data fetch" for the Google API client.
+         *
+         * @return A new {@link Sheets} service instance if the connection is successfully established,
+         *         {@code null} otherwise.
+         */
+        public static Sheets getService() {
+            try {
+               return new Sheets.Builder(
+                        GoogleNetHttpTransport.newTrustedTransport(),
+                        JacksonFactory.getDefaultInstance(),
+                        getCredentials())
+                        .setApplicationName("Test Data fetch")
+                        .build();
+            } catch (GeneralSecurityException | IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
          * Creates and returns a Google Sheets service.
          *
          * @return An instance of the Sheets service.
          */
-        private static Object getService() {
+        private static Object getService1() {
             try {
                 final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
                 return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName("Read Google Sheet").build();
-            } catch (GeneralSecurityException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
+            } catch (GeneralSecurityException | IOException e) {
                 e.printStackTrace();
             }
             return null;
@@ -181,7 +219,7 @@ public class DataProviderUtility {
          * @return A Sheets service for data operations.
          */
         private static Sheets getSheetService(){
-            return (Sheets) getService();
+            return getService();
         }
 
         /**
